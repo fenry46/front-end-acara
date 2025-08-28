ff2574b003cc20cba8df7fca4ff7242cdea9f1be
Fendy <76032813+fenry46@users.noreply.github.com>
2025-08-28 19:33:35 +0700

    Test: Rename back so the naming is correct

--- src/pages/admin/category/index.tsx
+++ src/pages/admin/category/index.tsx
@@ -1,5 +1,5 @@
 import DashboardLayout from "@/components/layouts/DashboardLayout";
-import Category from "@/components/views/Admin/Category-test";
+import Category from "@/components/views/Admin/Category";
 
 const AdminCategoryPage = () => {
   return (
