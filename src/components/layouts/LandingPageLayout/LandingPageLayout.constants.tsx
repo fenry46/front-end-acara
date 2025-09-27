d8e74e9ed4fda54e3d7a83fd27ca3cd3bb682f6b
Fendy <76032813+fenry46@users.noreply.github.com>
2025-09-27 21:40:32 +0700

    feat: footer UI

--- src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
@@ -1,3 +1,11 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+
 const NAV_ITEMS = [
   {
     label: "Home",
@@ -22,4 +30,32 @@ const BUTTON_ITEMS = [
   },
 ];
 
-export { NAV_ITEMS, BUTTON_ITEMS };
+const SOCIAL_ITEMS = [
+  {
+    label: "Facebook",
+    href: "https://facebook.com",
+    icon: <FaFacebook />,
+  },
+  {
+    label: "Instagram",
+    href: "https://instagram.com",
+    icon: <FaInstagram />,
+  },
+  {
+    label: "TikTok",
+    href: "https://Tiktok.com",
+    icon: <FaTiktok />,
+  },
+  {
+    label: "Twitter",
+    href: "https://twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    label: "Youtube",
+    href: "https://youtube.com",
+    icon: <FaYoutube />,
+  },
+];
+
+export { NAV_ITEMS, BUTTON_ITEMS, SOCIAL_ITEMS };
