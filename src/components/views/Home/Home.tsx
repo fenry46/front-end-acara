1b5b447ca436f4719ec7d3bd4ace97746b9fbe03
Fendy <76032813+fenry46@users.noreply.github.com>
2025-12-06 18:40:44 +0700

    feat: Slider Banner and Card Event

--- src/components/views/Home/Home.tsx
+++ src/components/views/Home/Home.tsx
@@ -1,7 +1,46 @@
+import { Skeleton } from "@nextui-org/react";
+import HomeList from "./HomeList";
+import HomeSlider from "./HomeSlider";
+import useHome from "./useHome";
+import Image from "next/image";
+
 const Home = () => {
+  const {
+    dataBanners,
+    isLoadingBanners,
+    dataFeaturedEvents,
+    isLoadingFeaturedEvents,
+    dataLatestEvents,
+    isLoadingLatestEvents,
+  } = useHome();
   return (
     <div>
-      <div>Home</div>
+      <HomeSlider
+        banners={dataBanners?.data}
+        isLoadingBanners={isLoadingBanners}
+      />
+      <HomeList
+        title="Featured Event"
+        events={dataFeaturedEvents?.data}
+        isLoading={isLoadingFeaturedEvents}
+      />
+      <Skeleton
+        isLoaded={!isLoadingBanners}
+        className="my-8 h-[20vw] w-full rounded-2xl"
+      >
+        <Image
+          src={dataBanners && `${dataBanners?.data[1]?.image}`}
+          alt="banner"
+          className="h-[20vw] w-full rounded-2xl object-cover object-center"
+          width={1920}
+          height={800}
+        />
+      </Skeleton>
+      <HomeList
+        title="Latest Event"
+        events={dataLatestEvents?.data}
+        isLoading={isLoadingLatestEvents}
+      />
     </div>
   );
 };
---
+++ src/components/views/Home/useHome.tsx
@@ -0,0 +1,54 @@
+import {
+  LIMIT_BANNER,
+  LIMIT_EVENT,
+  PAGE_DEFAULT,
+} from "@/constants/list.constants";
+import bannerServices from "@/services/banner.service";
+import eventServices from "@/services/event.service";
+import { useQuery } from "@tanstack/react-query";
+
+const useHome = () => {
+  const getBanners = async () => {
+    let params = `limit=${LIMIT_BANNER}&page=${PAGE_DEFAULT}`;
+    const res = await bannerServices.getBanners(params);
+    const { data } = res;
+    return data;
+  };
+
+  const { data: dataBanners, isLoading: isLoadingBanners } = useQuery({
+    queryKey: ["Banners"],
+    queryFn: getBanners,
+  });
+
+  const getEvents = async (params: string) => {
+    const res = await eventServices.getEvents(params);
+    const { data } = res;
+    return data;
+  };
+
+  const currentEventQuery = `limit=${LIMIT_EVENT}&page=${PAGE_DEFAULT}&isPublished=true`;
+
+  const { data: dataFeaturedEvents, isLoading: isLoadingFeaturedEvents } =
+    useQuery({
+      queryKey: ["FeaturedEvents"],
+      queryFn: () => getEvents(`${currentEventQuery}&isFeatured=true`),
+    });
+
+  const { data: dataLatestEvents, isLoading: isLoadingLatestEvents } = useQuery(
+    {
+      queryKey: ["LatestEvents"],
+      queryFn: () => getEvents(currentEventQuery),
+    },
+  );
+
+  return {
+    dataBanners,
+    isLoadingBanners,
+    dataFeaturedEvents,
+    isLoadingFeaturedEvents,
+    dataLatestEvents,
+    isLoadingLatestEvents,
+  };
+};
+
+export default useHome;
